66ea0dfe80179bc94bd0731c95464ddf8c2ba4d2
Denny Park <denny.park93@gmail.com>
2018-02-10 17:07:15 -0800

    add game routes

--- server/server.js
+++ server/server.js
@@ -14,8 +14,62 @@ app.use(cookieParser());
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE);
 
+// GET ALL GAMES
+app.get('/api/games', (req,res) =>{
+  let skip = parseInt(req.query.skip);
+  let limit = parseInt(req.query.limit);
+  let order = req.query.order;
+
+  Game.find().skip(skip).sort({_id:order}).limit(limit).exec((err,doc) => {
+    if (err) return res.status(400).send(err);
+    res.send(doc);
+  })
+})
+
+// GET GAME BY ID
+app.get('/api/getGame', (req,res) => {
+  let id = req.query.id;
+
+  Game.findById(id, (err,doc) => {
+    if (err) return res.status(400).send(err);
+    res.send(doc)
+  })
+})
+
+// POST GAME
+app.post('/api/game', (req, res) => {
+	const game = new Game(req.body);
+
+	game.save((err, doc) => {
+		if (err) return res.status(400).send(err);
+		res.status(200).json({
+			post: true,
+			bookId: doc._id
+		});
+	});
+});
+
+// UPDATE GAME
+app.post('/api/game_update',(req,res) => {
+  Game.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err,doc) => {
+    if (err) return res.status(400).send(err);
+    res.json({
+      success: true,
+      doc
+    })
+  })
+})
+
+// DELETE GAME
+app.delete('/api/game_delete',(req,res)=>{
+  let id = req.query.id;
+  Game.findByIdAndRemove(id,(err,doc)=>{
+    if (err) return res.status(400).send(err);
+    res.json(true)
+  })
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`Server running at ${port}`)
-})
+	console.log(`Server running at ${port}`);
+});
