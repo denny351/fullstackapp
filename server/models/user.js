a06b9f55da632355f5d07fa863c88ee9d2612e08
Denny Park <denny.park93@gmail.com>
2018-02-10 18:34:06 -0800

    add users routes (login/register with jwt)

--- server/models/user.js
+++ server/models/user.js
@@ -5,33 +5,70 @@ const config = require('./../config/config').get(process.env.NODE_ENV);
 const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: 1
-  },
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  },
-  name: {
-    type: String,
-    maxlength: 100,
-  },
-  lastname: {
-    type: String,
-    maxlength: 100
-  },
-  role: {
-    type: Number,
-    default: 0
-  },
-  token: {
-    type: String
-  }
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		unique: 1
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	name: {
+		type: String,
+		maxlength: 100
+	},
+	lastname: {
+		type: String,
+		maxlength: 100
+	},
+	role: {
+		type: Number,
+		default: 0
+	},
+	token: {
+		type: String
+	}
 });
 
+userSchema.pre('save', function(next) {
+	var user = this;
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(SALT_I, function(err, salt) {
+			if (err) return next(err);
+
+			bcrypt.hash(user.password, salt, function(err, hash) {
+				if (err) return next(err);
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+	var user = this;
+	bcrypt.compare(candidatePassword, user.password, function(err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+userSchema.methods.generateToken = function(cb) {
+	var user = this;
+	var token = jwt.sign(user._id.toHexString(), config.SECRET);
+
+	user.token = token;
+	user.save(function(err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
+
 const User = mongoose.model('User', userSchema);
-module.exports = { User }
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -14,29 +14,32 @@ app.use(cookieParser());
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE);
 
-// GET ALL GAMES
-app.get('/api/games', (req,res) =>{
-  let skip = parseInt(req.query.skip);
-  let limit = parseInt(req.query.limit);
-  let order = req.query.order;
+// GET
+app.get('/api/games', (req, res) => {
+	let skip = parseInt(req.query.skip);
+	let limit = parseInt(req.query.limit);
+	let order = req.query.order;
 
-  Game.find().skip(skip).sort({_id:order}).limit(limit).exec((err,doc) => {
-    if (err) return res.status(400).send(err);
-    res.send(doc);
-  })
-})
+	Game.find()
+		.skip(skip)
+		.sort({ _id: order })
+		.limit(limit)
+		.exec((err, doc) => {
+			if (err) return res.status(400).send(err);
+			res.send(doc);
+		});
+});
 
-// GET GAME BY ID
-app.get('/api/getGame', (req,res) => {
-  let id = req.query.id;
+app.get('/api/getGame', (req, res) => {
+	let id = req.query.id;
 
-  Game.findById(id, (err,doc) => {
-    if (err) return res.status(400).send(err);
-    res.send(doc)
-  })
-})
+	Game.findById(id, (err, doc) => {
+		if (err) return res.status(400).send(err);
+		res.send(doc);
+	});
+});
 
-// POST GAME
+// POST
 app.post('/api/game', (req, res) => {
 	const game = new Game(req.body);
 
@@ -49,25 +52,55 @@ app.post('/api/game', (req, res) => {
 	});
 });
 
+app.post('/api/register', (req, res) => {
+	const user = new User(req.body);
+	user.save((err, doc) => {
+		if (err) return res.json({ success: false });
+		res.status(200).json({
+			success: true,
+			user: doc
+		});
+	});
+});
+
+app.post('/api/login', (req, res) => {
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (err) return res.json({ isAuth: false, message: 'Auth failed, email not found' });
+
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if (!isMatch) return res.json({ isAuth: false, message: 'Wrong password' });
+
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err);
+				res.cookie('auth', user.token).json({ 
+          isAuth: true, 
+          id: user._id, 
+          email: user.email 
+        });
+			});
+		});
+	});
+});
+
 // UPDATE GAME
-app.post('/api/game_update',(req,res) => {
-  Game.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err,doc) => {
-    if (err) return res.status(400).send(err);
-    res.json({
-      success: true,
-      doc
-    })
-  })
-})
+app.post('/api/game_update', (req, res) => {
+	Game.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
+		if (err) return res.status(400).send(err);
+		res.json({
+			success: true,
+			doc
+		});
+	});
+});
 
 // DELETE GAME
-app.delete('/api/game_delete',(req,res)=>{
-  let id = req.query.id;
-  Game.findByIdAndRemove(id,(err,doc)=>{
-    if (err) return res.status(400).send(err);
-    res.json(true)
-  })
-})
+app.delete('/api/game_delete', (req, res) => {
+	let id = req.query.id;
+	Game.findByIdAndRemove(id, (err, doc) => {
+		if (err) return res.status(400).send(err);
+		res.json(true);
+	});
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
