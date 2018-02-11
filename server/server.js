4e374dc042326cef6a8c50fc0c01d3ff3bddd063
Denny Park <denny.park93@gmail.com>
2018-02-10 16:02:31 -0800

    initial setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE);
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
+})
